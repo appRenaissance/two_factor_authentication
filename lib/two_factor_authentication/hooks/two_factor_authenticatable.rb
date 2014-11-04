56bcfa608b5e5cc5a2b8f4d7705e7612a59a5d7b
atroutt <audrey.troutt@gmail.com>
2014-11-04 16:59:29 -0500

    manually handle sending two factor code at the right time.

--- lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
@@ -1,7 +1,8 @@
 Warden::Manager.after_authentication do |user, auth, options|
   if user.respond_to?(:need_two_factor_authentication?)
     if auth.session(options[:scope])[TwoFactorAuthentication::NEED_AUTHENTICATION] = user.need_two_factor_authentication?(auth.request)
-      user.send_two_factor_authentication_code
+      # we are manually handling sending the code
+      # user.send_two_factor_authentication_code
     end
   end
 end
